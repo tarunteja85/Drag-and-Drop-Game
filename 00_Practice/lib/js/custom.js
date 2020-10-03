a52f5f058bc3a1a6c8447c034dfde0d44fbde571
tarunteja85 <65776939+tarunteja85@users.noreply.github.com>
2020-10-03 19:07:28 +0530

    Added practice game

---
+++ 00_Practice/lib/js/custom.js
@@ -0,0 +1,22 @@
+﻿
+function myEvaluateFunction() {
+	var dragDropObject = dragdrop.getElementByType("custom")._custom;
+	var droppedCount = 0;
+	var correctlyDroppedCount = 0;
+	var redherringDroppedCount = 0;
+	console.log(dragDropObject._arDrags)
+	for(var i=0;i<dragDropObject._arDrags.length;i++){
+		var drag = dragDropObject._arDrags[i];
+		console.log("drag item " + drag.elementId + "; dropped: " + drag.dropped + "; match: " + drag.match);
+		if(drag.dropped){
+			droppedCount++;
+			if(drag.match === "-1") redherringDroppedCount++;
+			if(drag.match === drag.dropped.id) correctlyDroppedCount++
+			console.log(drag.container.innerHTML)
+		}
+	}
+
+	console.log("droppedCount=" +droppedCount)
+	console.log("correctlyDroppedCount=" +correctlyDroppedCount)
+	console.log("redherringDroppedCount=" +redherringDroppedCount)
+}
